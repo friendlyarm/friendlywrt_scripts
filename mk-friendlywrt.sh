34d37dabc19c192d0cd44373bab09bfea53f2f5c
Lawrence-Tang <tangzongsheng@gmail.com>
2021-10-31 12:57:49 +0800

    format code

--- mk-friendlywrt.sh
+++ mk-friendlywrt.sh
@@ -5,8 +5,8 @@ set -eu
 SCRIPTS_DIR=$(cd `dirname $0`; pwd)
 if [ -h $0 ]
 then
-        CMD=$(readlink $0)
-        SCRIPTS_DIR=$(dirname $CMD)
+    CMD=$(readlink $0)
+    SCRIPTS_DIR=$(dirname $CMD)
 fi
 cd $SCRIPTS_DIR
 cd ../
@@ -28,25 +28,25 @@ if [ ! -f .config ]; then
         readonly CURRPATH
         touch ${CURRPATH}/.config
         (cd ${TOP_DIR}/configs/${TARGET_FRIENDLYWRT_CONFIG} && {
-			for FILE in $(ls); do
-				if [ -f ${FILE} ]; then
+            for FILE in $(ls); do
+                if [ -f ${FILE} ]; then
                     echo "# apply ${FILE} to .config"
-					cat ${FILE} >> ${CURRPATH}/.config
-				fi
-			done
+                    cat ${FILE} >> ${CURRPATH}/.config
+                fi
+            done
         })
     else
         cp ${TOP_DIR}/configs/${TARGET_FRIENDLYWRT_CONFIG} .config
     fi
-	make defconfig
+    make defconfig
 else
-	echo "using .config file"
+    echo "using .config file"
 fi
 
 if [ ! -d dl ]; then
-	# FORTEST
-	# cp -af /opt4/openwrt-full-dl ./dl
-	echo "dl directory doesn't  exist. Will make download full package from openwrt site."
+    # FORTEST
+    # cp -af /opt4/openwrt-full-dl ./dl
+    echo "dl directory doesn't  exist. Will make download full package from openwrt site."
 fi
 make download -j$(nproc)
 find dl -size -1024c -exec ls -l {} \;
@@ -58,7 +58,7 @@ echo "${USING_DATE}" > ./package/base-files/files/etc/rom-version
 make -j$(nproc)
 RET=$?
 if [ $RET -eq 0 ]; then
-	exit 0
+    exit 0
 fi
 
 make -j1 V=s
